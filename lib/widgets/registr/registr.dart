e63507a51b0a3f5e1f5255aa28b32cbbaacb0ace
Arcticsniper1990 <Arcticsniper1990@gmail.com>
2022-07-21 22:10:48 +1000

    registr.dart update

--- lib/widgets/registr/registr.dart
+++ lib/widgets/registr/registr.dart
@@ -11,10 +11,11 @@ class _RegistrWidgetState extends State<RegistrWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(children: [
-          SizedBox(height: 40),
+      backgroundColor: Colors.grey.shade900,
+      body: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SizedBox(height: 30),
           Text(
             'VKid',
             style: TextStyle(color: Colors.white, fontSize: 40),
@@ -27,20 +28,44 @@ class _RegistrWidgetState extends State<RegistrWidget> {
           SizedBox(height: 10),
           Text(
             'Ваш номер телефона будет использоваться для входа в аккаунт',
-            style: TextStyle(color: Colors.white, fontSize: 20) , textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.grey.shade500, fontSize: 20),
+            textAlign: TextAlign.center,
           ),
-          TextField(),
-          SizedBox(height: 350),
-          ElevatedButton(style: ElevatedButton.styleFrom(
-                primary: Colors.white, minimumSize: Size.fromHeight(40)),
-              onPressed: () {}, child: Text('Продолжить',  style: TextStyle(color: Colors.black,),)),
-          Text(
-            'Нажимаю продолжить вы принимаете',
-            style: TextStyle(color: Colors.white, fontSize: 12), textAlign: TextAlign.center,
+          SizedBox(height: 20),
+          TextField(
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: '  Номер телефона',
+                hintStyle: TextStyle(color: Colors.grey.shade500, fontSize: 15),
+                enabled: true),
           ),
-          Text(
-            'пользовательское соглашение и политику конфидециальности.',
-            style: TextStyle(color: Colors.white, fontSize: 12), textAlign: TextAlign.center,
+          SizedBox(height: 50),
+          Container(
+            alignment: Alignment.bottomCenter,
+            // height: double.infinity,
+            child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.white, minimumSize: Size.fromHeight(40)),
+                  onPressed: () {},
+                  child: Text(
+                    'Продолжить',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  )),
+              Text(
+                'Нажимаю продолжить вы принимаете',
+                style: TextStyle(color: Colors.grey.shade500, fontSize: 12),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'пользовательское соглашение и политику конфидециальности.',
+                style: TextStyle(color: Colors.white, fontSize: 12),
+                textAlign: TextAlign.center,
+              ),
+            ]),
           ),
         ]),
       ),
